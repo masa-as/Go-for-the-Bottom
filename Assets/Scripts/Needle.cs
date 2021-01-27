a0b7e65eb3fe4a742dbbc815ebd59af4540fc833
masashi-iijima <65524819+masashi-iijima@users.noreply.github.com>
2021-01-27 17:28:43 +0900

    crazyモード作成
    ファイル分割

---
+++ Assets/Scripts/Needle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Needle : MonoBehaviour {
+
+	Rigidbody2D rigidbody2D;
+	public GameObject UnityChan;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		//UnityChanとぶつかった時
+		if (col.gameObject.tag == "Player") {
+			//LifeScriptのLifeDownメソッドを実行
+			SceneManager.LoadScene ("Gameover");
+		}
+	}
+
+}
